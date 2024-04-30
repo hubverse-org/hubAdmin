31d10bd4696d2e2aeab99b4a3df9e2a558b9b39e
Anna Krystalli <annakrystalli@googlemail.com>
2024-04-30 19:13:25 +0300

    Remove example to silence note

--- R/utils.R
+++ R/utils.R
@@ -5,11 +5,6 @@
 #'
 #' @return The schema version number as a character string.
 #' @export
-#'
-#' @examples
-#' extract_version_n(
-#'   "https://raw.githubusercontent.com/Infectious-Disease-Modeling-Hubs/schemas/main/v3.0.0/tasks-schema.json"
-#' )
 extract_version_n <- function(id) {
   stringr::str_extract(id, "v([0-9]\\.){2}[0-9](\\.[0-9]+)?")
 }
