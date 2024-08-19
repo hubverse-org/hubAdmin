da1f53d267306d7f777c9e3d790d2b44e94115fe
Zhian N. Kamvar <zkamvar@gmail.com>
2024-08-19 13:14:12 -0700

    add test for failing CI test

--- tests/testthat/test-ci_validate_hub_config.R
+++ tests/testthat/test-ci_validate_hub_config.R
@@ -27,6 +27,8 @@ test_that("ci_validate_hub creates message of success", {
   expect_match(tail(diff1, 1), "NOW")
   expect_snapshot(writeLines(diff1))
   
+  # Second run overwrites the file ------------------------------
+  expect_true(fs::file_exists(fs::path(simp, "diff.md")))
   # the results should be true
   local_mocked_bindings(timestamp = broken_clock("LATER"))
   expect_true(
@@ -42,6 +44,54 @@ test_that("ci_validate_hub creates message of success", {
   expect_match(diff2[1], "correct")
   expect_match(tail(diff2, 1), "LATER")
   expect_snapshot(writeLines(diff2))
+
+})
+
+test_that("ci_validate_hub creates message of failure", {
+  # generator to overwrite the `timestamp()` function with known stamps
+  broken_clock <- function(when = "NOW") {
+    function(path) {
+      cat(when, "\n", file = path, append = TRUE)
+    }
+  }
+  tmp <- withr::local_tempdir()
+  fs::dir_copy(testthat::test_path("testdata", "error_hub") , tmp)
+  out  <- withr::local_tempfile()
+  err <- fs::path(tmp, "error_hub")
+  # the diff file should not exist
+  expect_false(fs::file_exists(fs::path(err, "diff.md")))
+  # the results should be false 
+  local_mocked_bindings(timestamp = broken_clock("NOW"))
+  expect_false(
+    ci_validate_hub_config(hub_path = err, gh_output = out) %>%
+      unlist() %>%
+      all() %>%
+      suppressMessages()
+  )
+
+  # the output file exists and contains the 
+  expect_true(fs::file_exists(fs::path(err, "diff.md")))
+  diff1 <- readLines(fs::path(err, "diff.md"))
+  expect_match(diff1[1], "Invalid Configuration")
+  expect_match(tail(diff1, 1), "NOW")
+  # NOTE: not attempting to snapshot the generated table because its ID and
+  # the file path changes all the time and testthat's masking is flaky on CI
+  # systems that tend to change their tempdir specifictions. 
+  
+  # the results should be false 
+  local_mocked_bindings(timestamp = broken_clock("LATER"))
+  expect_false(
+    ci_validate_hub_config(hub_path = err, gh_output = out) %>%
+      unlist() %>%
+      all() %>%
+      suppressMessages()
+  )
+
+  # the output file exists
+  expect_true(fs::file_exists(fs::path(err, "diff.md")))
+  diff2 <- readLines(fs::path(err, "diff.md"))
+  expect_match(diff2[1], "Invalid Configuration")
+  expect_match(tail(diff2, 1), "LATER")
 
 })
 
