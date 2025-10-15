d04d42f9675f2261460d4eba5b25f2e4aa41c4da
Anna Krystalli <annakrystalli@googlemail.com>
2025-10-15 11:24:03 -0400

    Test that target data config files are validated through validate_hub_config

--- tests/testthat/test-validate_hub_config.R
+++ tests/testthat/test-validate_hub_config.R
@@ -1,10 +1,12 @@
 test_that("correct hub validates successfully", {
   skip_if_offline()
   expect_true(
-    validate_hub_config(hub_path = system.file(
-      "testhubs/simple/",
-      package = "hubUtils"
-    )) %>%
+    validate_hub_config(
+      hub_path = system.file(
+        "testhubs/simple/",
+        package = "hubUtils"
+      )
+    ) %>%
       unlist() %>%
       all() %>%
       suppressMessages()
@@ -15,7 +17,8 @@ test_that("Hub with config errors fails validation", {
   skip_if_offline()
   val <- validate_hub_config(
     hub_path = testthat::test_path(
-      "testdata", "error_hub"
+      "testdata",
+      "error_hub"
     )
   ) %>%
     suppressWarnings()
@@ -36,4 +39,20 @@ test_that("Hub with config errors fails validation", {
     attr(val, "schema_version"),
     "v2.0.0"
   )
+})
+
+test_that("Target data files handled appropriately", {
+  hub_path <- system.file("testhubs/v6/target_file/", package = "hubUtils")
+  expect_message(
+    hub_val <- validate_hub_config(
+      hub_path = hub_path
+    ),
+    regexp = "target-data.json" # Ensure target-data.json is mentioned in message.
+  )
+  expect_s3_class(hub_val, "hubval")
+  expect_named(
+    hub_val,
+    c("tasks", "admin", "target-data", "model-metadata-schema")
+  )
+  expect_true(all(unlist(hub_val)))
 })
